a7b3d41923ff393f54e80c648554af80d9d6d68d
manchandajayant <maanjayant@gmail.com>
2020-04-27 01:17:29 +0200

    rune added

--- sketch.js
+++ sketch.js
@@ -15,21 +15,25 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = x / (count - 1);
         const v = y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v) * 0.025);
         points.push({
           color: random.pick(palette),
           position: [u, v],
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          radius: radius,
+          // radius: Math.abs(0.01 + random.gaussian() * 0.01),
         });
       }
     }
     return points;
   };
   const margin = 400;
-  random.setSeed(40);
+
   const points = createGrid().filter(() => random.value() * 100 < 50);
   console.log("points", points);
   return ({ context, width, height }) => {
-    context.fillStyle = "#c7c5c3";
+    const palette = random.shuffle(random.pick(palettes)).slice(1, 4);
+    colorForFill = random.pick(palette);
+    context.fillStyle = colorForFill;
     context.fillRect(0, 0, width, height);
     points.forEach((data) => {
       const { position, radius, color } = data;
@@ -37,11 +41,14 @@ const sketch = () => {
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.lineWidth = 5;
+      // context.fill();
       context.fillStyle = color;
-      context.lineWidth = 5;
-      context.fill();
+      context.font = "100px Helvetica";
+      context.fillText("ᚠ", x, y);
     });
   };
 };
